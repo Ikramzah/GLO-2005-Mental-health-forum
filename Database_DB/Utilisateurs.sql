6a6d190692625a80b86fc67cd32ed51648c2e8a4
Ikram zahiri <ikram.zahiri.1@ulaval.ca>
2025-03-26 09:30:54 -0400

    Correction du conflit du dernier commit

--- Database_DB/Utilisateurs.sql
+++ Database_DB/Utilisateurs.sql
@@ -1,4 +1,3 @@
--- Table Utilisateurs
 CREATE TABLE Utilisateurs (
     username VARCHAR(50)  PRIMARY KEY,
     nom VARCHAR(50) NOT NULL,
@@ -11,14 +10,14 @@ CREATE TABLE Utilisateurs (
     lieu_de_residence VARCHAR(100)
 );
 
--- Table Conseillers
+-- Table Conseillers 
 CREATE TABLE Conseillers (
     username VARCHAR(50) PRIMARY KEY,
     specialisation VARCHAR(100),
     FOREIGN KEY (username) REFERENCES Utilisateurs(username) ON DELETE CASCADE
 );
 
--- Table Etudiants
+-- Table Etudiants 
 CREATE TABLE Etudiants (
     username VARCHAR(50) PRIMARY KEY,
     statut_etudiant Enum('international', 'en echange', 'citoyen', 'resident permanent') NOT NULL,
@@ -26,13 +25,12 @@ CREATE TABLE Etudiants (
     FOREIGN KEY (username) REFERENCES Utilisateurs(username) ON DELETE CASCADE
 );
 
--- Table Moderateur
+-- Table Moderateur 
 CREATE TABLE Moderateur (
     username VARCHAR(50) PRIMARY KEY,
     FOREIGN KEY (username) REFERENCES Utilisateurs(username) ON DELETE CASCADE
 );
 
-<<<<<<< HEAD
 
 -- INSERT des 30 étudiants
 INSERT INTO Utilisateurs (username, nom, prenom, email, mot_de_passe, date_inscription, date_de_naissance, lieu_de_residence)
@@ -133,30 +131,6 @@ VALUES
 ('mod03');
 
 -- verifier l'age
-=======
--- Table Reserver
-CREATE TABLE Reserver (
-    id_rdv INT AUTO_INCREMENT PRIMARY KEY,
-    username_conseiller VARCHAR(50),
-    username_etudiant VARCHAR(50),
-    date DATE NOT NULL,
-    heure_debut TIME NOT NULL,
-    heure_fin TIME NOT NULL,
-    statut ENUM('confirme', 'annule', 'en attente') DEFAULT 'en attente',
-    FOREIGN KEY (username_conseiller) REFERENCES Conseillers(username) ON DELETE CASCADE,
-    FOREIGN KEY (username_etudiant) REFERENCES Etudiants(username) ON DELETE CASCADE
-);
-
--- Contraintes supplémentaires
-ALTER TABLE Reserver
-ADD CONSTRAINT chk_heure
-CHECK (heure_debut < heure_fin);
-
-ALTER TABLE Reserver
-ADD CONSTRAINT chk_date_futur
-CHECK (date >= CURRENT_DATE());
-
->>>>>>> a956263 (index)
 ALTER TABLE Utilisateurs
 ADD CONSTRAINT chk_age_minimum
 CHECK (
@@ -164,37 +138,8 @@ CHECK (
   OR date_de_naissance IS NULL
 );
 
-<<<<<<< HEAD
 
-=======
--- Trigger pour éviter les doubles réservations
-DELIMITER $$
-CREATE TRIGGER trig_prevent_double_booking
-BEFORE INSERT ON Reserver
-FOR EACH ROW
-BEGIN
-    IF NEW.heure_debut >= NEW.heure_fin THEN
-        SIGNAL SQLSTATE '45000'
-            SET MESSAGE_TEXT = 'Heure de début doit être strictement inférieure à heure de fin.';
-    END IF;
 
-    IF EXISTS (
-        SELECT 1
-        FROM Reserver
-        WHERE date = NEW.date
-          AND (
-               username_conseiller = NEW.username_conseiller
-               OR username_etudiant = NEW.username_etudiant
-              )
-          AND (NEW.heure_debut < heure_fin AND NEW.heure_fin > heure_debut)
-    )
-    THEN
-        SIGNAL SQLSTATE '45000'
-            SET MESSAGE_TEXT = 'Chevauchement détecté : le conseiller ou l\'étudiant a déjà un rendez-vous sur ce créneau.';
-    END IF;
-END$$
-DELIMITER ;
->>>>>>> a956263 (index)
 
 -- Indexation pour optimisation du système
 CREATE INDEX idx_utilisateurs_email ON Utilisateurs(email);
