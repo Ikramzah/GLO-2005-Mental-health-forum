0b45fe2886b6e4b42c2b135435eefa7155d79ac6
Samah Naji <samah.naji.1@ulaval.ca>
2025-04-06 17:31:07 -0400

    home page

--- Database_DB/db_config.py
+++ Database_DB/db_config.py
@@ -4,7 +4,7 @@ def get_connection():
     return pymysql.connect(
         host='localhost',
         user='root',
-        password='xxxxx',
+        password='xxxx',
         database='MENTALHEALTH_DB',
         cursorclass=pymysql.cursors.DictCursor
     )
