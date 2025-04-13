8d86b69de72cbee61c9d8171785b25bbd8ec5db0
Samah <152223956+SANAJ12s@users.noreply.github.com>
2025-04-13 02:40:22 -0400

    a rebase

--- Database_DB/db_config.py
+++ Database_DB/db_config.py
@@ -4,11 +4,7 @@ def get_connection():
     return pymysql.connect(
         host='localhost',
         user='root',
-<<<<<<< HEAD
-        password='Kimo.2004',
-=======
-        password='Mamafirst1',
->>>>>>> 7f0c3b3 (authentification, modification de mot de pass, styling de modifier mot de pass, modifier photo et toucher un peu la page profil, Ajout d'un formulaire réservé au conseiller pour ajouter un livre)
+        password='xxxx',
         database='MENTALHEALTH_DB',
         cursorclass=pymysql.cursors.DictCursor
     )
