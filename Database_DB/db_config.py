bc898308129a9528c598c5e852220b03dd7dd97e
Samah Naji <samah.naji.1@ulaval.ca>
2025-04-13 17:17:36 -0400

    interface de modification uniques, livres ajoutés par conseillers

--- Database_DB/db_config.py
+++ Database_DB/db_config.py
@@ -4,7 +4,7 @@ def get_connection():
     return pymysql.connect(
         host='localhost',
         user='root',
-        password='xxxx',
+        password='Mamafirst1',
         database='MENTALHEALTH_DB',
         cursorclass=pymysql.cursors.DictCursor
     )
